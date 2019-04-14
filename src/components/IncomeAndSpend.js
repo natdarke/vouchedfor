175c199b8849980b0ffbb4efc6c2fdcbdc7ec001
Nat Darke <nat@natdarke.com>
2019-04-14 21:53:31 +0100

    Converted IncomeAndSpend and SpendLess components from classes to functions

--- src/components/IncomeAndSpend.js
+++ src/components/IncomeAndSpend.js
@@ -1,33 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../css/IncomeAndSpend.css';
 import FieldSet from './FieldSet';
 
-class IncomeAndSpend extends Component {
-  render() {
-    return (
-      <section className="s income-and-spend">
-        <header className="c income-and-spend">
-          <h1>Your Income & Spend</h1>
-        </header>
-        <div className="c income-and-spend user">
-          <form>
+
+function  IncomeAndSpend(props) {
+  return (
+    <section className="s income-and-spend">
+      <header className="c income-and-spend">
+        <h1>Your Income & Spend</h1>
+      </header>
+      <div className="c income-and-spend user">
+        <form>
+        <div>
+            <h2>Annual</h2>
+            {props.root.state.incomes.map(
+                (item, index) => <FieldSet {...item} key={index} onChangeHandler={props.root.setIncomes}/>
+            )}
+          </div>
           <div>
-              <h2>Annual</h2>
-              {this.props.root.state.incomes.map(
-                  (item, index) => <FieldSet {...item} key={index} onChangeHandler={this.props.root.setIncomes}/>
-              )}
-            </div>
-            <div>
-              <h2>Monthly spending</h2>
-              {this.props.root.state.expenditures.map(
-                  (item, index) => <FieldSet {...item} key={index} onChangeHandler={this.props.root.setExpenditures}/>
-              )}
-            </div>
-          </form>
-        </div>
-      </section>
-    );
-  }
+            <h2>Monthly spending</h2>
+            {props.root.state.expenditures.map(
+                (item, index) => <FieldSet {...item} key={index} onChangeHandler={props.root.setExpenditures}/>
+            )}
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default IncomeAndSpend;
--- src/components/SpendLess.js
+++ src/components/SpendLess.js
@@ -1,42 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../css/SpendLess.css';
 import Range from './Range.js';
 
-class SpendLess extends Component {
-  render() {
-    return (
-      <section className="s spend-less">
-        <header className="c spend-less">
-          <h1>Spend Less</h1>
-        </header>
-        <div className="c spend-less intro text">
-          <p>Try reducing your monthly spend to see how your forecast could improve</p>
-        </div>
-        <div className="c spend-less user">
-          <form>
-            <Range name='mortgage' label='Mortgage'
-              value={this.props.root.state.expenditures[0].amount} min='0' max='1500' 
-              changeHandler={this.props.root.setExpenditures} />
-            <Range name='bills' label='Bills'
-              value={this.props.root.state.expenditures[1].amount} min='0' max='1500' 
-              changeHandler={this.props.root.setExpenditures} />
-            <Range name='general spending' label='General Spending'
-              value={this.props.root.state.expenditures[2].amount} min='0' max='1500' 
-              changeHandler={this.props.root.setExpenditures} />
-            <output>This means you're saving <span>£{this.props.root.state.saving}</span> per month</output>
-          </form>
-        </div>
-        <div className="c spend-less link">
-          <a href="http://www.google.com?q=find+ways+to+save" target="_blank" rel="noopener noreferrer">Find ways to save</a>
-        </div>
-        <div className="c spend-less feedback">
-          <span className="question">Was this helpful?</span>
-          <a href="/" className="answer yes"><span>Yes</span></a>
-          <a href="/" className="answer no"><span>No</span></a>
-        </div>
-      </section>
-    );
-  }
+function SpendLess(props) {
+  return (
+    <section className="s spend-less">
+      <header className="c spend-less">
+        <h1>Spend Less</h1>
+      </header>
+      <div className="c spend-less intro text">
+        <p>Try reducing your monthly spend to see how your forecast could improve</p>
+      </div>
+      <div className="c spend-less user">
+        <form>
+          <Range name='mortgage' label='Mortgage'
+            value={props.root.state.expenditures[0].amount} min='0' max='1500' 
+            changeHandler={props.root.setExpenditures} />
+          <Range name='bills' label='Bills'
+            value={props.root.state.expenditures[1].amount} min='0' max='1500' 
+            changeHandler={props.root.setExpenditures} />
+          <Range name='general spending' label='General Spending'
+            value={props.root.state.expenditures[2].amount} min='0' max='1500' 
+            changeHandler={props.root.setExpenditures} />
+          <output>This means you're saving <span>£{props.root.state.saving}</span> per month</output>
+        </form>
+      </div>
+      <div className="c spend-less link">
+        <a href="http://www.google.com?q=find+ways+to+save" target="_blank" rel="noopener noreferrer">Find ways to save</a>
+      </div>
+      <div className="c spend-less feedback">
+        <span className="question">Was this helpful?</span>
+        <a href="/" className="answer yes"><span>Yes</span></a>
+        <a href="/" className="answer no"><span>No</span></a>
+      </div>
+    </section>
+  );
 }
 
 export default SpendLess;
