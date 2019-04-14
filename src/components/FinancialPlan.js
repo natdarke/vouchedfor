10aeaf5e32f8a1799a67249a0ebe79f570e0f078
Nat Darke <nat@natdarke.com>
2019-04-14 19:56:27 +0100

    Added local storage for state persistence

--- src/components/FinancialPlan.js
+++ src/components/FinancialPlan.js
@@ -7,62 +7,75 @@ class FinancialPlan extends Component {
 
   constructor(){
     super();
-    this.state = {
-        incomes: [
-          {
-            amount: 45300,
-            from_age: 30,
-            to_age: 67,
-            frequency: 'annual',
-            name: 'Annual salary'
-          }
-        ],
-        expenditures: [
-          {
-            amount: 1199,
-            from_age: 30,
-            to_age: 65,
-            frequency: 'monthly',
-            name: 'Mortgage'
-          },
-          {
-            amount: 1199,
-            from_age: 30,
-            to_age: 65,
-            frequency: 'monthly',
-            name: 'Bills'
-          },
-          {
-            amount: 1199,
-            from_age: 30,
-            to_age: 65,
-            frequency: 'monthly',
-            name: 'General spending'
-          }
-        ],
-        saving: this.calculateSaving()
+    const initialState = {
+      incomes: [
+        {
+          amount: 45300,
+          from_age: 30,
+          to_age: 67,
+          frequency: 'annual',
+          name: 'Annual salary'
+        }
+      ],
+      expenditures: [
+        {
+          amount: 1199,
+          from_age: 30,
+          to_age: 65,
+          frequency: 'monthly',
+          name: 'Mortgage'
+        },
+        {
+          amount: 1199,
+          from_age: 30,
+          to_age: 65,
+          frequency: 'monthly',
+          name: 'Bills'
+        },
+        {
+          amount: 1199,
+          from_age: 30,
+          to_age: 65,
+          frequency: 'monthly',
+          name: 'General spending'
+        }
+      ],
+      saving: this.calculateSaving()
+    }
+    if (
+      typeof(Storage) !== "undefined"
+      && typeof(sessionStorage.financialPlanState) !== "undefined"
+    ) {
+      this.state = JSON.parse(sessionStorage.financialPlanState);
+    }
+    else {
+      this.state = initialState;
     }
   }
 
   setExpenditures = event => {
     const { value, name } = event.target;
-    this.setState((state) => {
-      let { expenditures } = {...state};
-      expenditures = expenditures.map(
-        (item) => {
-          if(item.name.toUpperCase() === name.toUpperCase()) {
-            item.amount = value;
+    this.setState(
+      (state) => {
+        let { expenditures } = {...state};
+        expenditures = expenditures.map(
+          (item) => {
+            if(item.name.toUpperCase() === name.toUpperCase()) {
+              item.amount = value;
+            }
+            return item;
           }
-          return item;
+        )
+        return {
+          expenditures,
+          saving : this.calculateSaving()
         }
-      )
-      return {
-        expenditures
+      },
+      () => {
+        sessionStorage.setItem("financialPlanState", JSON.stringify(this.state));
       }
-    });
-    this.setState({
-      saving : this.calculateSaving()
-    })
+    );
+    
   }
   calculateSaving(){
     return 0;
@@ -74,7 +87,7 @@ class FinancialPlan extends Component {
         <header className="c financial-plan">
           <h1>Your Financial Plan</h1>
         </header>
-        <IncomeAndSpend root={this}/>
+        <IncomeAndSpend root={this} />
         <SpendLess root={this}/>
       </section>
     );
